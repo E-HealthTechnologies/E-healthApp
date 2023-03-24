237c8a24a5ac2a9cb75d658bf1d8891f1625a385
sifou <blvsifo3@gmail.com>
2023-03-25 00:49:51 +0100

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       // theme: ThemeData(
       //   inputDecorationTheme: const InputDecorationTheme(
       //     filled: true, //<-- SEE HERE
