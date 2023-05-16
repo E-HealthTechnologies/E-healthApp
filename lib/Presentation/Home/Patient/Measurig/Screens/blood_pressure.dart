52636838499e85a3148dade8596a269e2cb82413
the1der <djeltimedamine@gmail.com>
2023-05-16 17:03:35 +0100

    select data by day

--- lib/Presentation/Home/Patient/Measurig/Screens/blood_pressure.dart
+++ lib/Presentation/Home/Patient/Measurig/Screens/blood_pressure.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+
 import 'package:e_health/Presentation/Home/Patient/Measurig/bloc/pat_measuring_bloc.dart';
+import 'package:e_health/Services/GetDataServices.dart';
+import 'package:e_health/Services/StoreDataServices.dart';
+import 'package:e_health/Services/graph_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -24,34 +29,35 @@ class _BloodPressureScreenState extends State<BloodPressureScreen> {
   @override
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> data;
-    List<SalesData> dataList = [];
-
+    List<GlucoseTimedData> smallList = [];
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(
-          'Click to start measuring',
-          style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 15.sp,
-            color: Colors.white,
-          ),
-        ),
-        SizedBox(height: 20.sp),
+        // Text(
+        //   'Click to start measuring',
+        //   style: TextStyle(
+        //     fontWeight: FontWeight.w400,
+        //     fontSize: 15.sp,
+        //     color: Colors.white,
+        //   ),
+        // ),
+        // SizedBox(height: 20.sp),
         ElevatedButton(
           onPressed: () async {
-            // GetDataServices service = GetDataServices();
-            // data = await service.getMeasurementData(
-            //     uid: widget.uid, type: 'Glucose');
-            // print(data.length);
-            // for (int i = 0; i < data.length; i++) {
-            //   dataList.add(
-            //       SalesData(value: data[i]['Value'], date: data[i]['Date']));
-            // }
-            //
-            // _patMeasuringBloc.add(GetDataEvent(data: data, dataList: dataList));
-            // print(dataList.length);
-
+            GetDataServices service = GetDataServices();
+            data = await service.getMeasurementData(
+                uid: widget.uid, type: 'Glucose');
+            print(data.length);
+            smallList = GraphService.glucoDayData(
+                bigList: TimedData(data),
+                dateTime: DateTime.parse("2023-07-02 10:50:40.056570"));
+            print(smallList.length.toString());
+            for (int i = 0; i < smallList.length; i++) {
+              log(smallList[i].value.toString());
+            }
+            _patMeasuringBloc
+                .add(GetDataEvent(data: data, dataList: smallList));
+            print(smallList.length);
             /** **************** */
             // for (int i = 1; i < 50; i++) {
             //   var rnd = Random();
@@ -62,7 +68,7 @@ class _BloodPressureScreenState extends State<BloodPressureScreen> {
             //     // 'Date': DateFormat.yMd()
             //     //     .add_jm()
             //     //     .format(DateTime.now().add(Duration(days: i))),
-            //     'Date' : DateTime.now().add(Duration(days: i , hours: i)),
+            //     'Date': DateTime.now().add(Duration(days: i, hours: i)),
             //     'Value': value,
             //     // 'Value big': value~/2,
             //   };
@@ -93,28 +99,54 @@ class _BloodPressureScreenState extends State<BloodPressureScreen> {
             ),
           ),
         ),
-        SizedBox(height: 20.sp),
+        SizedBox(height: 40.sp),
         Container(
-          child: SfCartesianChart(
-            backgroundColor: Colors.white,
-            primaryXAxis: CategoryAxis(),
-            series: <LineSeries<SalesData, String>>[
-              LineSeries<SalesData, String>(
-                dataSource: _patMeasuringBloc.state.dataList,
-                xValueMapper: (SalesData sales, _) => sales.date,
-                yValueMapper: (SalesData sales, _) => sales.value,
+          child: Container(
+            color: Color(0x00f5f5f5),
+            // color: Colors.white,
+            child: SfCartesianChart(
+              borderWidth: 0,
+              plotAreaBorderWidth: 0,
+
+              // Initialize category axis
+              primaryXAxis: DateTimeAxis(
+                // minimum: DateTime.parse("2023-05-16 13:50:33.396256"),
+                // maximum: DateTime.parse("2023-07-05 13:50:40.059106"),
+                axisLine: AxisLine(color: Color(0x00000000)),
+                majorGridLines: MajorGridLines(
+                  color: Color(0x00000000),
+                ),
               ),
-            ],
-            // primaryYAxis: ,
+              primaryYAxis: NumericAxis(
+                // interval: 10,
+                // minimum: 50,
+                // maximum: 130,
+                axisLine: AxisLine(color: Color(0x00000000)),
+                majorGridLines: MajorGridLines(
+                  color: Color(0x0F000000),
+                ),
+              ),
+              // primaryXAxis: CategoryAxis(),
+              palette: <Color>[Colors.red],
+
+              series: <ChartSeries<GlucoseTimedData, DateTime>>[
+                LineSeries<GlucoseTimedData, DateTime>(
+                    dataSource: _patMeasuringBloc.state.dataList,
+                    xValueMapper: (GlucoseTimedData sales, _) =>
+                        sales.timeStamp,
+                    yValueMapper: (GlucoseTimedData sales, _) => sales.value,
+                    color: Colors.amber
+                    // gradient: LinearGradient(
+                    //   colors: [Color.fromARGB(255, 255, 0, 0), Color(0x0Fffffff)],
+                    //   begin: Alignment.topCenter,
+                    //   end: Alignment.bottomCenter,
+                    // ),
+                    ),
+              ],
+            ),
           ),
         ),
       ],
     );
   }
-}
-
-class SalesData {
-  SalesData({required this.value, required this.date});
-  final String date;
-  final int value;
 }
